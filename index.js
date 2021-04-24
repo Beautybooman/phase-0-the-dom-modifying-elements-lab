bc3c595ce9eb4887426fbb5a2a33cce36525c505
Isaiah Davis <ID@isaiahs-imac.myfiosgateway.com>
2021-04-24 19:34:03 -0400

    commit tree ca61389014d156fb66196093124b9ac6731a4903

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your code here!
+
+
+//const main = document.getElementsbyTagName('body')[0];
+//main.removeChild(body.querySelector())
+
+const first = document.getElementsByTagName('body')[0];
+first.removeChild(first.querySelector('main'))
+
+const newHeader = document.createElement('h1')
+newHeader.id = "victory"
+newHeader.innerHTML = "Isaiah is the champion"
+newHeader.className = "victory"
+
+// const newHeader2 = document.createElement('h1');
+// newHeader.innerHTML = "<h1>victory <h1>"
